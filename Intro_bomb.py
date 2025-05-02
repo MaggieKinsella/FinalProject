c304d33b4168d3edfc0abcc7abb053a822963d82
MaggieKinsella <mkinsella123@icloud.com>
2025-05-02 13:26:36 -0400

    Add files via upload

---
+++ Intro_bomb.py
@@ -0,0 +1,49 @@
+#INTRO SCREEN BEFORE BOMB
+
+import tkinter as tk
+
+# Create main window
+root = tk.Tk()
+root.title("Black Text Screen")
+root.configure(bg="black")
+root.geometry("600x400")
+
+# Create a read-only text display,
+text_display = tk.Text(root, bg="black", fg="white", font=("Courier", 30), state='disabled', wrap = "word")
+text_display.pack(expand=True, fill='both', padx=10, pady=10)
+
+# Subroutine which gives type writer class which takes in text and delay and prints it depending on what you give it
+def typewriter(text, delay=100, linebreak=True):
+    def inner(i=0):
+        if i < len(text):
+            text_display.configure(state='normal')
+            text_display.insert(tk.END, text[i])
+            text_display.configure(state='disabled')
+            text_display.see(tk.END)
+            root.after(delay, inner, i+1)
+        elif linebreak:
+            text_display.configure(state='normal')
+            text_display.insert(tk.END, "\n")
+            text_display.configure(state='disabled')
+            text_display.see(tk.END)
+    inner()
+    
+    # Clear screen and show new message
+def intro_phase1():
+    text_display.configure(state='normal')
+    text_display.delete('1.0', tk.END)
+    text_display.configure(state='disabled')
+    typewriter("PHASE 1", delay=100)
+    root.after(2000, lambda: typewriter("OBJECTIVE: Eat all the Pac-Dots scattered throughout the maze to complete the level. Avoid the ghosts, contact with a ghost results in losing a life.", delay = 100))
+
+#NEW OBJECTIVE: Eat all the Pac-Dots scattered throughout the maze to complete the level. Avoid the ghosts: Contact with a ghost results in losing a life.
+
+#INTRO TO WHOLE BOMB DEFUSION
+typewriter("DEFUSE THE BOMB", delay=100)
+root.after(2500, lambda: typewriter("OBJECTIVE: COMPLETE ALL 4 PHASES QUICKLY IN ORDER TO DEFUSE THE BOMB. OR ELSE...", delay=80))
+
+# After 15 seconds (15000 ms), switch to new screen
+root.after(15000, intro_phase1)
+
+# Run the GUI
+root.mainloop()
