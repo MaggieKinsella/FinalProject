f656b7a8db37f86a130d90fe15fd1e87c7c97c52
MaggieKinsella <mkinsella123@icloud.com>
2025-05-03 12:35:00 -0400

    Update Maggie_bomb.py

--- Maggie_bomb.py
+++ Maggie_bomb.py
@@ -175,7 +175,7 @@ class Wires(PhaseThread):
             #self.lcd.after(0, lambda: self.lcd._lwires.config(text=f"Wires: {self._value}"))
             if self._value == self.correct_value:
                 self.lcd.wires_done = True
-                self.lcd._lwires.config(text="Wires Complete")
+                self.lcd.after(0, lambda: self.lcd._lwires.config(text="Complete!"))
                 self.lcd.check_all_phases_complete()
                 break
             sleep(0.1)
